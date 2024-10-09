63a47d706e1fea9ecbdb10d6dc4961bb0ea05164
farisKomo <farisedu9@gmail.com>
2024-10-09 18:27:45 +0300

    MVC

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const dotenv = require('dotenv');
-
 dotenv.config();
 
 const app = require('./app');
+
+const DB = process.env.DATABASE.replace('<PASSWORD>',process.env.DATABASE_PASSWORD);
+mongoose.connect(DB, {
+  useNewUrlParser:true,
+  useCreateIndex:true,
+  useFindAndModify:false
+}).then(con => {
+  console.log('Database Connected Succefully');
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {

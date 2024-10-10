ddb355c416c39cfc728eb91a8ff35f1200e78225
farisKomo <farisedu9@gmail.com>
2024-10-10 17:22:45 +0300

    New Changes

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,27 +1,31 @@
-const fs = require('fs');
 const Tour = require('../models/tourModel');
 
+exports.getAllTOurs = async (req, res) => {
+  try {
+    const tours = await Tour.find();
 
-exports.getAllTOurs = (req, res) => {
-  console.log(req.requestTime);
-  res.status(200).send({
-    status: 'succes',
-    // results: tours.length,
-    // data: { tours },
-  });
+    res.status(200).send({
+      status: 'succes',
+      results: tours.length,
+      data: { tours },
+    });
+  } catch (error) {
+    res.status(404).json({ status: 'Failed', message: error.message });
+  }
 };
 
-exports.getTour = (req, res) => {
-  const id = req.params.id * 1;
-  // const tour = tours.find((el) => el.id === id);
-  // if (!tour) {
-  //   return res.status(404).json({ status: 'Failed', message: 'invalid id' });
-  // }
-  // res.status(200).send({ status: 'succes', data: { tour } });
+exports.getTour = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const tour = await Tour.findById(id);
+    res.status(200).send({ status: 'succes', data: { tour } });
+  } catch (error) {
+    res.status(404).json({ status: 'Failed', message: error.message });
+  }
 };
 
-exports.createTour = (req, res) => {
-
+exports.createTour = async (req, res) => {
+  const newTour = await Tour.create(req.body);
 
   res.status(201).json({
     status: 'Success',
@@ -29,21 +33,25 @@ exports.createTour = (req, res) => {
   });
 };
 
-exports.updateTour = (req, res) => {
-  const id = req.params.id * 1;
-  if (!tour) {
-    return res.status(404).json({ status: 'Failed', message: 'invalid id' });
+exports.updateTour = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const tour = await Tour.findByIdAndUpdate(id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    return res.status(200).json({ status: 'Success', data: { tour } });
+  } catch (error) {
+    res.status(404).json({ status: 'Failed', message: error.message });
   }
-  return res
-    .status(200)
-    .json({ status: 'Success', data: { tour: 'Updated tour here' } });
 };
 
-exports.deleteTour = (req, res) => {
-  const id = req.params.id * 1;
-  const tour = 'Something' ;
-  if (!tour) {
-    return res.status(404).json({ status: 'Failed', message: 'invalid id' });
+exports.deleteTour = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const tour = await Tour.findByIdAndDelete(id);
+    return res.status(204).json({ status: 'Success', data: null });
+  } catch (error) {
+    res.status(404).json({ status: 'Failed', message: error.message });
   }
-  return res.status(204).json({ status: 'Success', data: null });
 };
--- server.js
+++ server.js
@@ -1,16 +1,22 @@
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 dotenv.config();
 
 const app = require('./app');
 
-const DB = process.env.DATABASE.replace('<PASSWORD>',process.env.DATABASE_PASSWORD);
-mongoose.connect(DB, {
-  useNewUrlParser:true,
-  useCreateIndex:true,
-  useFindAndModify:false
-}).then(con => {
-  console.log('Database Connected Succefully');
-});
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then((con) => {
+    console.log('Database Connected Succefully');
+  });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
